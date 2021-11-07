885e6d9002e89dabe2b424ee1abd923660faa74f
Alex Eagleson <alexeagleson@gmail.com>
2021-11-06 20:45:09 -0400

    remove console log

--- file.js
+++ file.js
@@ -1,32 +1,42 @@
-const dotenv = require('dotenv').config()
-const CronJob = require('cron').CronJob;
-const axios = require('axios')
+const dotenv = require("dotenv").config();
+const CronJob = require("cron").CronJob;
+const axios = require("axios");
 
-let recipes = ["lasagna", "lettuce wraps", "shepherds pie", "burgers", "just candy"];
+let recipes = [
+  "lasagna",
+  "lettuce wraps",
+  "shepherds pie",
+  "burgers",
+  "just candy",
+];
 
 const recipeSelector = () => {
-    return recipes[Math.floor(Math.random()*recipes.length)];
-}
+  return recipes[Math.floor(Math.random() * recipes.length)];
+};
 
-console.log('Before job instantiation');
-var job = new CronJob('* * * * * *', function() {
-  sendFoodToDiscord()
-}, null, true, 'America/Los_Angeles');
-console.log('After job instantiation');
-job.start();
+console.log("Before job instantiation");
+var job = new CronJob(
+  "* * * * * *",
+  function () {
+    sendFoodToDiscord();
+  },
+  null,
+  true,
+  "America/Los_Angeles"
+);
+console.log("After job instantiation");
+// job.start();
 
-let sendFoodToDiscord = () => {
-axios
-  .post(process.env.DA_HOOK, {
-    content: 
-    "Don't forget this tasty nugget: " +
-    recipeSelector(recipes)
-  })
-  .then(res => {
-    console.log(`statusCode: ${res.status}`)
-    console.log(res)
-  })
-  .catch(error => {
-    console.error(error)
-  })
-}
+var sendFoodToDiscord = () => {
+  axios
+    .post(process.env.DA_HOOK, {
+      content: "Don't forget this tasty nugget: " + recipeSelector(recipes),
+    })
+    .then((res) => {
+      console.log(`statusCode: ${res.status}`);
+      // console.log(res)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
